66ddbd5e4c358339418a106b3ff2642e255ef8f8
FlowState App <info@theflowstateapp.com>
2025-09-08 00:24:43 +0530

    Create minimal JSON response function

--- api/demo/index.js
+++ api/demo/index.js
@@ -1,9 +1,3 @@
-// Vercel Serverless Function - Demo Overview
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=300');
-  res.setHeader('X-Robots-Tag', 'index,follow');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  
-  res.status(200).send('<html><body><h1>Demo Overview</h1><p>This is a test.</p></body></html>');
+  res.status(200).json({ message: 'Demo overview working' });
 }
